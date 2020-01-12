6a5957e224e11584e4c42d29dbad4ae9c949d4bc
actoure <acherif.toure@gmail.com>
2020-01-12 14:17:49 +0100

    initlizing home fragment in the activity

--- app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/presentation/MainActivity.kt
@@ -15,7 +15,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        
+
+        /* initialize fragment */
+        replaceFragment(HomeFragment.newInstance())
+
         setupNavigationElements()
     }
 
