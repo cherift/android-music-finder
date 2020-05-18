b32181078db09de7907761754d2cccf54367835a
actoure <acherif.toure@gmail.com>
2020-05-18 23:01:00 +0200

    feat: informations screen

--- app/src/main/java/com/example/myapplication/presentation/fragment/InfoFragment.kt
+++ app/src/main/java/com/example/myapplication/presentation/fragment/InfoFragment.kt
@@ -1,9 +1,11 @@
 package com.example.myapplication.presentation.fragment
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 
@@ -12,6 +14,8 @@ import com.example.myapplication.R
  * Info fragment class
  */
 class InfoFragment : Fragment() {
+
+    var rootView : View? = null
 
     companion object {
         fun newInstance() : InfoFragment = InfoFragment()
@@ -22,6 +26,11 @@ class InfoFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.frangment_infos, container, false)
+        rootView = inflater.inflate(R.layout.frangment_infos, container, false)
+
+        val githubLink : TextView = rootView!!.findViewById(R.id.github_link)
+        githubLink.movementMethod = LinkMovementMethod.getInstance()
+
+        return rootView
     }
 }
